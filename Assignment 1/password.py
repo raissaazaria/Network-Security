f388fc8c955a66af10278dba56af0a6c9e39a049
Raissa Azaria <91534032+raissaazaria@users.noreply.github.com>
2024-08-16 22:52:07 +1000

    Delete Assignment 1/password.py

--- Assignment 1/password.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-username = "Bob"
-pw = "password123"
-hashed_pw = hashlib.sha1(pw.encode()).hexdigest()
-
-with open("Alice/password.txt", "w") as f:
-    f.write(f"{username}:{hashed_pw}\n")
